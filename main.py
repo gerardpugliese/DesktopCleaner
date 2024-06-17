e01a1f22a6a01dc851d92869fc7b39b5b83f3566
unknown <pugliesegerard@gmail.com>
2024-06-17 16:09:36 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+~ Desktop Cleaner ~
+
+1. What should this program do?
+    This program should automatically sort the files on your Desktop into predetermined folders.
+2. How will it achieve this?
+    To start, we will create folders for a defined list of file extensions. Then we will loop through the
+    files on our desktop and put them into their designated folder.
+3. How can we expand on this?
+    * Add a GUI 
+    * Allow for folders to be sorted as well as files
+    * Create custom sorting conditions beyond file extensions
+
+'''
+
+def main():
+    # Define list of extensions that we'll be sorting
+
+    # Loop through and create a file for each extension (if one doesn't exist already)
+
+    # Loop through files on desktop and put them into their respective folders
+
+    return 0
+
+
+
+if __name__ == "__main__":
+    main() 
