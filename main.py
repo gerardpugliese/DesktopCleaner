561ce0d22e5d24c593c5b89f76d50a762ea4a0b5
unknown <pugliesegerard@gmail.com>
2024-06-27 15:58:41 -0400

    feat: Implements stable version 1

--- main.py
+++ main.py
@@ -14,52 +14,97 @@
 '''
 import shutil
 import os
-from dotenv import load_dotenv, dotenv_values
 
-load_dotenv()
-
-PATH_TO_DESKTOP = os.getenv("PATH_TO_DESKTOP")
-
-def main():
-    print("-----Start of Sorting Script-----")
-    # Define list of folders that we'll be sorting into
-    #folders = ["Images", "Audio", "Docs"]
-    folders = {
-        "Images": "",
-        "Audio": "",
-        "Docs": ""
-    }
-    img_exts = ["jpg", "png", "jfif"]
-    audio_exts = ["mp3", "wav"]
-    doc_exts = ["docx"]
-
-    # Loop through and create a file for each extension (if one doesn't exist already)
-    for folder in folders:
-        folder_to_make = PATH_TO_DESKTOP + folder
-        if not os.path.exists(folder_to_make):
-            print("Making folder for: ", folder)
-            os.makedirs(folder_to_make)
-            folders[folder] = folder_to_make
-        else:
-            print(folder_to_make + " exists!")
-
-    # Loop through files on desktop and put them into their respective folders
-    desktop_files = os.listdir(PATH_TO_DESKTOP)
-    for file in desktop_files:
+def start_cleanup(path_to_source, rules):
+    # For each rule in rules, loop through files in source (path_to_source) and check for name or extension match
+    source_files = os.listdir(path_to_source)
+    for file in source_files:
         split_file = file.split(".")
-        if len(split_file) == 1: # This is a directory not a file
-            continue
-        else: # Check extension and move to appropriate folder
-            extension = split_file[1]
-            file_dir = PATH_TO_DESKTOP + file
-            if extension in img_exts:
-                shutil.move(file_dir, folders["Images"])
-            elif extension  in audio_exts:
-                shutil.move(file_dir, folders["Audio"])
-            elif extension in doc_exts:
-                shutil.move(file_dir, folders["Docs"])
-            else:
-                print(f"unsupported extension type: {extension}")
 
+        for dest in rules:
+            if rules[dest]['names'][0] != '': #Empty names
+                for name in rules[dest]['names']:
+                    print('rules[dest][names]: ', rules[dest]['names'])
+                    if name in split_file[0]:
+                        shutil.move(path_to_source+file, dest)
+                        continue
+            if len(split_file) > 1: # This is a file, check for extension match
+                if "."+split_file[1].strip() in rules[dest]['extensions']:
+                    shutil.move(path_to_source+"/"+file, dest)
+   
+def main():
+    rules = {}
+    done_with_rules = False
+    print("Welcome to File Cleanup!")
+
+    # Retrieve / verify source folder
+    source_path_valid = False
+    path_to_source = input("What folder would you like to cleanup? Ex: C:/Users/<username>/Desktop \n")
+    while not source_path_valid:
+        if not os.path.exists(path_to_source):
+            create_source = input("Folder doesn't exist, would you like to create it? (Y/N)")
+            if create_source.lower() == 'y':    
+                try:
+                    os.makedirs(path_to_source)
+                except OSError:
+                    path_to_source = input("Please enter a valid directory.\n")
+                else:
+                    source_path_valid = True
+            else:
+                exit()
+        else:
+            source_path_valid = True
+
+    # Create rules to filter by
+    while not done_with_rules:
+        print("Rule #" + str(len(rules)+1)+ ":")
+
+        # Retrieve / verify destination folder for this rule
+        rule_dest = input("What folder would you like these files put into?\n")
+        while not os.path.isdir(rule_dest):
+            rule_dest = input("Please enter a valid directory.\n")
+
+        # Initialize item for this rule
+        rules[rule_dest] = {}
+        rules[rule_dest]['extensions'] = []
+        rules[rule_dest]['names'] = [] 
+
+        # Retrieve extensions for this rule
+        temp_extensions = input("What file extensions do you want to be put into this folder? Ex: .jpg, .png \n")
+        temp_extensions = temp_extensions.split(',')
+        rules[rule_dest]['extensions'] = temp_extensions
+
+        # Retrieve names for this rule
+        temp_names = input("What file names do you want to be put into this folder? Ex: Screenshots, 062425\n")
+        temp_names = temp_names.split(',')
+        rules[rule_dest]['names'] = temp_names
+
+        # Loop again if we want to create another rule
+        more_rules = input("Would you like to create another Rule? (Y/N)")
+        if more_rules == "N":
+            done_with_rules = True
+    
+    # Once all rules are create, display them so user can confirm before starting
+    print("\nYour cleanup rules:")
+    print("--------------------\n")
+    rule_counter = 1
+    for dest in rules:
+        print("Rule #"  + str(rule_counter) +":")
+        print("Destination folder: " + dest)
+        print("Extensions: ", end="")
+        for ext in rules[dest]['extensions']:
+            print(ext.strip(), end=' ')
+        print("\nNames: ", end="")
+        for name in rules[dest]['names']:
+            print(name.strip(), end=' ')
+        rule_counter += 1
+        print("\n")
+    start_cleanup_input = input("\nStart cleanup with these rules? (Y/N)")
+    if start_cleanup_input == "Y" or start_cleanup_input == "y":
+        print("Start cleanup")
+        start_cleanup(path_to_source, rules)
+    elif start_cleanup_input == "N" or start_cleanup_input == "n":
+        print("Do not start cleanup")
+    
 if __name__ == "__main__":
     main() 
